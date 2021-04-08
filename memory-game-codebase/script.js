c5c85bafcf2d6fe697a7d0917856bfaa6d2d6359
zataara <bryantmac@gmail.com>
2021-04-07 21:30:28 -0700

    added current score functionality

--- memory-game-codebase/script.js
+++ memory-game-codebase/script.js
@@ -1,4 +1,10 @@
 const gameContainer = document.getElementById("game");
+let card1 = null;
+let card2 = null;
+let cardsFlipped = 0;
+let noClicking = false;
+let newGameButton = document.querySelector('#newGame')
+let currentScore = 0;
 
 const COLORS = [
   "red",
@@ -13,9 +19,8 @@ const COLORS = [
   "purple"
 ];
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
+
+
 function shuffle(array) {
   let counter = array.length;
 
@@ -43,38 +48,23 @@ let shuffledColors = shuffle(COLORS);
 // it also adds an event listener for a click for each card
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
-    // create a new div
     const newDiv = document.createElement("div");
-
-    // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
 }
 
-// TODO: Implement this function!
-let card1;
-let card2;
-let cardsFlipped = 0;
-let noClicking = false;
-
-
-
-function handleCardClick(event) {
-  if (event.target.classList.contains('flipped')){
-  return;
-  }
+function handleCardClick(e) {
+  if (noClicking) return;
+  if (e.target.classList.contains("flipped")) return;
 
   let currentCard = e.target;
   currentCard.style.backgroundColor = currentCard.classList[0];
 
   if (!card1 || !card2) {
     currentCard.classList.add("flipped");
+    setScore(currentScore + 1);
     card1 = card1 || currentCard;
     card2 = currentCard === card1 ? null : currentCard;
   }
@@ -108,5 +98,11 @@ function handleCardClick(event) {
   if (cardsFlipped === COLORS.length) alert("game over!");
 }
 
-// when the DOM loads
+
+
+function setScore(newScore) {
+  currentScore = newScore;
+  document.getElementById("counter").innerText = currentScore;
+}
+
 createDivsForColors(shuffledColors);
